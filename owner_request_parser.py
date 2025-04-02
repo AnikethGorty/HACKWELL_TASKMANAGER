343133202387cd7cac66ee96cd89d825eaf84efd
AnikethGorty <anikethgorty@gmail.com>
2025-04-03 00:25:40 +0530

    task allocation?

---
+++ owner_request_parser.py
@@ -0,0 +1,10 @@
+from huggingface_hub import InferenceClient
+import requests
+import os
+
+HF_KEY=os.getenv("HUGGINGFACE_TOKEN")
+
+client = InferenceClient(
+    provider="novita",
+    api_key=HF_KEY,
+)
